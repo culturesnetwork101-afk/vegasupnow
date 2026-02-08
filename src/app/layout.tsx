180fcd758dba621a45326f6001da88b6903f080a
I Am Joe Jack <joejackson80@gmail.com>
2026-02-07 19:29:46 -0600

    Fix: Restore and brighten lounge background using body pseudo-elements

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,8 +18,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="globalBackground" />
-        <div className="patternOverlay" />
         {children}
       </body>
     </html>
